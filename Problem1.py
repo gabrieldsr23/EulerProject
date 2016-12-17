4334550f1695b585f1a5a2b4262733935475ba53
gabrieldsr23 <gabrieldsr23@gmail.com>
2016-12-17 15:21:24 -0200

    Update Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -2,4 +2,4 @@
 # we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #Find the sum of all the multiples of 3 or 5 below 1000.
 
-print(sum([x for x in range(1000) if x % 3 == 0 and x % 5 == 0]))
+print(sum([x for x in range(1000) if x % 3 == 0 or x % 5 == 0]))
